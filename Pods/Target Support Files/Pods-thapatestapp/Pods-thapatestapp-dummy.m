0083959a6a704242bb6a5831566dfbec4d093d8f
Saurabh Sharma <saurabh@binaryinformatics.com>
2018-07-10 15:35:42 +0530

    okdddcfd3

---
+++ Pods/Target Support Files/Pods-thapatestapp/Pods-thapatestapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_thapatestapp : NSObject
+@end
+@implementation PodsDummy_Pods_thapatestapp
+@end
---
+++ thapatestapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thapatestapp
+//
+//  Created by ADMIN on 7/10/18.
+//  Copyright © 2018 ADMIN. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
