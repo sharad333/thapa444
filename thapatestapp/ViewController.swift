0a30ba8afef86170d8613ce2f32bd0397eeab62b
Saurabh Sharma <thapasharad333@gmail.com>
2018-07-12 11:56:54 +0530

    okkkokddDddddd

--- thapatestapp/ViewController.swift
+++ thapatestapp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("kkdkkd")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
