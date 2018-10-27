bbdd185e40e3837c24967be6022cc2c058f14989
sharad333 <thapsharad333@gmail.com>
2018-10-27 12:49:42 +0530

    fkjdfd

--- thapatestapp/AppDelegate.swift
+++ thapatestapp/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        print("jlkaklddd")
+        let iiii : Int = 0
+        print("jlkaklddd\(iiii)")
+        
         // Override point for customization after application launch.
         return true
     }
